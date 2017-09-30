dc3468c13e86fef4bac2df7ba8c340ee07e53975
Nicholas Bauer <nicholasbauer@outlook.com>
2017-09-30 15:05:51 -0400

    Nick done

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -9,148 +9,64 @@ class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-
-
             int sellIn = items[i].sellIn;
-            
-            int quality = computeQuality(items[i].name,items[i].sellIn,items[i].quality);
-            
 
-            int qualityChange = 0;
+            items[i].quality = computeQuality(items[i].name, items[i].sellIn, items[i].quality);
 
-            if (isAgedBrie) {
-
-            }
-            else if (isBackstagePass) {
-                if (sellIn < 11) {
-                    qualityChange = -2;
-                }
-                else if (sellIn < 6) {
-                    qualityChange = -3;
-                }
-                else if (sellIn < 0){
-                    qualityChange
-                }
-            }
-            else if (isSulfuras) {
-                qualityChange = 0;
-            }
-            else {
-                qualityChange = -1;
-            }
-
-
-
-
-            items[i].quality = Math.min(items[i].quality, 50);
-            items[i].quality = Math.max(items[i].quality, 0);
-
-
-
-
-            if (!items[i].name.contains().equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
-
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
-
-           /* if (!items[i].name.equals("Conjured")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }*/
-
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
-            }
+            items[i].sellIn -= 1;
         }
     }
     
-    public int computeQuality (String name, int sellIn, int quality){
+    public int computeQuality (String name, int sellIn, int quality) {
+        boolean isConjured = name.toLowerCase().contains("conjured");
+        boolean isAgedBrie = name.toLowerCase().contains("aged brie");
+        boolean isBackstagePass = name.toLowerCase().contains("backstage passes");
+        boolean isSulfuras = name.toLowerCase().contains("sulfuras");
 
-        boolean isConjured = items[i].name.toLowerCase().contains("Conjured");
-
-        boolean isAgedBrie = items[i].name.toLowerCase().contains("Aged Brie");
-        boolean isBackstagePass = items[i].name.toLowerCase().contains("Backstage passes");
-        boolean isSulfuras = items[i].name.toLowerCase().contains("Sulfuras");
+        if (isSulfuras)
+            return quality;
 
         int qualityChange = 0;
 
-            if (isAgedBrie) {
-                    if (sellIn < 0) {
-                            qualityChange = +2;
-                        } else {
-                            qualityChange = +1;
-                }
+        if (isAgedBrie) {
+            if (sellIn <= 0) {
+                qualityChange = +2;
+            } else {
+                qualityChange = +1;
+            }
+        }
+        else if (isBackstagePass) {
+            if (sellIn < 11) {
+                qualityChange = +2;
+            }
+            else if (sellIn < 6) {
+                qualityChange = +3;
+            }
+            else if (sellIn < 0){
+                qualityChange = -1 * quality;
+            } else
+            {
+                qualityChange = +1;
+            }
+        }
+        else {
+            if (sellIn <= 0) {
+                qualityChange = -2;
+            } else {
+                qualityChange = -1;
+            }
+        }
 
-            }
-            else if (isBackstagePass) {
-                if (sellIn < 11) {
-                    qualityChange = +2;
-                }
-                else if (sellIn < 6) {
-                    qualityChange = +3;
-                }
-                else if (sellIn < 0){
-                    qualityChange = -1 * quality;
-                } else
-                {
-                    qualityChange = +1;
-                }
-            }
-            else if (isSulfuras) {
-                qualityChange = 0;
-            }
-            else {
-                if (sellIn < 0) {
-                    qualityChange = -2;
-                } else {
-                    qualityChange = -1;
-                }
-            }
-            if(isConjured && qualityChange < 0){
+        if(isConjured && qualityChange < 0){
+            qualityChange = qualityChange * 2;
+        }
 
-                qualityChange = qualityChange * 2;
-            }
-        if !isSulfuras
-        return 0;
+        int newQuality = quality + qualityChange;
+
+        newQuality = Math.min(newQuality, 50);
+        newQuality = Math.max(newQuality, 0);
+
+        return newQuality;
         
     }
 }
